36c9e92a54bd6193020f3f33b85cef1e722ea661
VinayTShetty <vinaytshetty@gmail.com>
2023-06-08 16:31:10 +0530

    Variable accessing inside the class.Both static and instance

---
+++ CoreJava/Variables/AcessingVariables.java
@@ -0,0 +1,34 @@
+class Test
+{
+	public static void method_m1(){
+		//This is static area
+	}
+
+	public void method_2(){
+		// This is instance area
+	}
+}
+
+
+Acessing Variables
+******************
+Inside the class
+Outside the class
+
+
+
+Inside the class-Instance variables
+***********************************
+1)Inside the Instance Area Directly.
+2)OutSide the Instance Area using Object Reference.
+
+Inside the class-Static variables 
+********************************
+1)Directly we can access inside the method.(Both instance/static area)
+2)Using class reference also we can access in instance/static area.
+3)Using Object reference we can access instance variables.
+
+Local Variables-Acess
+*********************
+Local variables should be accessed within the scope of the method,block,constructuor,loop.
+We can acess the local variables directly with out any additional help.
