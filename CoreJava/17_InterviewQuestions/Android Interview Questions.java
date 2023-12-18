00768ee30d4f27e4c57c6a76447cef849434dc82
VinayTShetty <vinaytshetty@gmail.com>
2023-12-18 12:27:57 +0530

    Android Interview Questions

---
+++ CoreJava/17_InterviewQuestions/Android Interview Questions.java
@@ -0,0 +1,39 @@
+https://www.interviewbit.com/android-interview-questions/
+
+Components of Android application
+Android application architecture has the list of components given below. They are as follows:
+
+Services - It will be used to perform background functionalities.
+Activities - It is a single screen that represents GUI(Graphical User Interface) with which users can interact in order to do something like dial the phone, view email, etc.
+Broadcast receivers - Broadcast receiver is a mechanism used for listening to system-level events like listening for incoming calls, SMS, etc. by the host application.
+Content providers - This will share the data between various applications.
+*****************************************************************************************************************************
+
+What are the features of Android architecture?
+*************************************************************************************************
+What is an activity?
+Activity in java is a single screen that represents GUI(Graphical User Interface) with which users can interact in order to do something like dial the phone, view email, etc.
+*************************************************************************************************
+What is a service in Android?
+*************************************************************************************************
+ Differentiate Activities from Services.
+*************************************************************************************************
+. What is the use of Bundle in Android?
+*************************************************************************************************
+What is an Adapter in Android?
+*************************************************************************************************
+What is the life cycle of Android activity?
+*************************************************************************************************
+What is AndroidManifest.xml file and why do you need this?
+*************************************************************************************************
+What is an intent?
+explain implicit Intent and Explicit Intent.
+*************************************************************************************************
+What is a Toast? Write its syntax.
+*************************************************************************************************
+ What is context?
+*************************************************************************************************
+What is ANR in Android? What are the measures you can take to avoid ANR?
+*************************************************************************************************
+What are broadcast receivers? How is it implemented?
+*************************************************************************************************
